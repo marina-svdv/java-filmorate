ab7a6e9eff27535c6247bbedf2214cf57672295d
marina-svdv <marina.svrdova@yandex.ru>
2023-11-03 15:25:37 +0300

    rm space2

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 public class Film {
@@ -15,7 +16,6 @@ public class Film {
     private LocalDate releaseDate;
     private int duration;
     private int rate;
-
     {
         if (id == 0) {
             id = COUNTER++;
