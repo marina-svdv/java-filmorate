655ee0663fa01368b4dd8689dd0889a54cbc75b1
marina-svdv <marina.svrdova@yandex.ru>
2023-12-19 16:48:59 +0300

    fix: in UserDbStorageTest

---
+++ target/classes/data.sql
@@ -0,0 +1,12 @@
+MERGE INTO mpa (name) KEY(name) VALUES ('G');
+MERGE INTO mpa (name) KEY(name) VALUES ('PG');
+MERGE INTO mpa (name) KEY(name) VALUES ('PG-13');
+MERGE INTO mpa (name) KEY(name) VALUES ('R');
+MERGE INTO mpa (name) KEY(name) VALUES ('NC-17');
+
+MERGE INTO genres (name) KEY(name) VALUES ('Комедия');
+MERGE INTO genres (name) KEY(name) VALUES ('Драма');
+MERGE INTO genres (name) KEY(name) VALUES ('Мультфильм');
+MERGE INTO genres (name) KEY(name) VALUES ('Триллер');
+MERGE INTO genres (name) KEY(name) VALUES ('Документальный');
+MERGE INTO genres (name) KEY(name) VALUES ('Боевик');
